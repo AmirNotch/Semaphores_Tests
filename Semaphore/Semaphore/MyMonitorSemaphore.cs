c1e04b85020a4c83e5aceab0287a7d6ecaa3e441
aibragimov <Ibragimov-amir@list.ru>
2024-05-26 23:03:16 +0500

    Tests commit

---
+++ Semaphore/Semaphore/MyMonitorSemaphore.cs
@@ -0,0 +1,65 @@
+﻿namespace Semaphore.Semaphore;
+
+public class MyMonitorSemaphore : ISemaphore.ISemaphore
+{
+    private readonly object _lock = new object();
+    private int _count;
+    private readonly int _initialCount;
+
+    public MyMonitorSemaphore(int count)
+    {
+        if (count < 0)
+            throw new ArgumentOutOfRangeException(nameof(count));
+        
+        _count = count;
+        _initialCount = count;
+    }
+
+    public void Acquire()
+    {
+        lock (_lock)
+        {
+            while (_count == 0)
+            {
+                Monitor.Wait(_lock);
+            }
+
+            _count--;
+        }
+    }
+
+    public bool TryAcquire()
+    {
+        lock (_lock)
+        {
+            if (_count > 0)
+            {
+                _count--;
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public int Release(int releaseCount)
+    {
+        if (releaseCount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(releaseCount));
+
+        lock (_lock)
+        {
+            int previousCount = _count;
+            _count += releaseCount;
+
+            if (_count > _initialCount)
+            {
+                _count = _initialCount;
+                throw new SemaphoreFullException("Превышение максимального счетчика.");
+            }
+
+            Monitor.PulseAll(_lock);
+            return previousCount;
+        }
+    }
+}
---
+++ SemaphoreTest/MySemaphoreTest.cs
@@ -0,0 +1,61 @@
+using Semaphore.ISemaphore;
+using Semaphore.Semaphore;
+
+namespace SemaphoreTest;
+
+[TestClass]
+public class MySemaphoreTests
+{
+    [TestMethod]
+    public void TestAcquire()
+    {
+        ISemaphore semaphore = new MySemaphore(1);
+        semaphore.Acquire();
+        bool result = semaphore.TryAcquire();
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void TestTryAcquire_Success()
+    {
+        ISemaphore semaphore = new MySemaphore(1);
+        bool result = semaphore.TryAcquire();
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void TestTryAcquire_Failure()
+    {
+        ISemaphore semaphore = new MySemaphore(1);
+        semaphore.Acquire();
+        bool result = semaphore.TryAcquire();
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void TestRelease()
+    {
+        ISemaphore semaphore = new MySemaphore(1);
+        semaphore.Acquire();
+        semaphore.Release(1);
+        bool result = semaphore.TryAcquire();
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void TestReleaseMultipleTimes()
+    {
+        ISemaphore semaphore = new MySemaphore(1);
+        semaphore.Acquire();
+        int previousCount = semaphore.Release(1); // Один Семафор, не превышаем максимум
+        Assert.AreEqual(0, previousCount); // До освобождения, count = 0
+        semaphore.Acquire();
+        previousCount = semaphore.Release(1); // Второй Семафор
+        Assert.AreEqual(0, previousCount);
+        semaphore.Acquire();
+        previousCount = semaphore.Release(1); // Третий Семафор
+        Assert.AreEqual(0, previousCount);
+        bool result = semaphore.TryAcquire();
+        Assert.IsTrue(result); // После освобождения можно подхватить семафор
+    }
+}
